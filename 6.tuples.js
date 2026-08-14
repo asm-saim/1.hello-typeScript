e822b32ab9814a4805d252a3ee1e6a42dfcfc5e4
asm-saim <saim777m@gmail.com>
2026-08-14 17:20:22 +0600

    tuples implemented

---
+++ 6.tuples.js
@@ -0,0 +1,4 @@
+"use strict";
+//Tuples: A fixed length array, where each element has it's own type.
+let info = ["Alice", 23];
+console.log(info);
---
+++ 6.tuples.ts
@@ -0,0 +1,4 @@
+//Tuples: A fixed length array, where each element has it's own type.
+
+let info: [string, number] = ["Alice", 23];
+console.log(info);
