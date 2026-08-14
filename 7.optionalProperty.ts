ca0cf5cbcebc35940527af6abf749da06e317301
asm-saim <saim777m@gmail.com>
2026-08-14 21:13:12 +0600

    literals implemented

--- 7.optionalProperty.ts
+++ 7.optionalProperty.ts
@@ -2,13 +2,17 @@
 const school: {
   name: string;
   established: number;
+  location: "Naval2"; //literals
   isRunning: boolean;
   student?: number;
 } = {
   name: "NPSN",
   established: 2003,
+  location: "Naval2",
   isRunning: true,
   //   student: 500,
 };
 
 console.log(school);
+
+//literals: constant value that can not be changed.
