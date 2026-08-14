d8fc0f4a6bfe6872fb4a499f6b2ad015ab95444b
asm-saim <saim777m@gmail.com>
2026-08-14 10:46:37 +0600

    primitive data types in TS

---
+++ 3.types.js
@@ -0,0 +1,21 @@
+"use strict";
+//Primitive Datatypes in TS: SNBBUSN
+// The main primitive types are:
+// string
+// number
+// boolean
+// bigint
+// symbol
+// undefined
+// null
+//string
+let district = "Dhaka";
+district = "Rajshahi";
+// district = 404;
+//number:
+let students = 300;
+let price = 23.6;
+let value = -2;
+//Boolean:
+let isRaining = true;
+console.log(district, students, price, value, isRaining);
---
+++ 3.types.ts
@@ -0,0 +1,26 @@
+//Primitive Datatypes in TS: SNBBUSN
+// The main primitive types are:
+// string
+// number
+// boolean
+// bigint
+// symbol
+// undefined
+// null
+
+//string
+let district: string = "Dhaka";
+district = "Rajshahi";
+// district = 404;
+
+//number:
+let students: number = 300;
+let price: number = 23.6;
+let value: number = -2;
+
+//Boolean:
+let isRaining: boolean = true;
+
+console.log(district, students, price, value, isRaining);
+
+
