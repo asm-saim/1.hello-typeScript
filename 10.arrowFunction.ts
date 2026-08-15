ad6b7fa6fc6cad14758c2f1764794378f7ffbb45
asm-saim <saim777m@gmail.com>
2026-08-15 15:46:56 +0600

    arrow function implemented

---
+++ 10.arrowFunction.ts
@@ -0,0 +1,7 @@
+const biggerName = (name1: string, name2: string): string => {
+  if (name1.length > name2.length) {
+    return name1;
+  }
+  return name2;
+};
+const res = biggerName("Wasi", "JK");
