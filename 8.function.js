026b0414d24edf0b29ed183b37bde03a4d07e739
asm-saim <saim777m@gmail.com>
2026-08-15 14:47:16 +0600

    function syntax in TS

---
+++ 8.function.js
@@ -0,0 +1,8 @@
+"use strict";
+//function syntax:
+function sum(num1, num2) {
+    const total = num1 + num2;
+    return total;
+}
+const result = sum(6, 4);
+console.log(result);
---
+++ 8.function.ts
@@ -0,0 +1,9 @@
+//function syntax:
+
+function sum(num1: number, num2: number): number {
+  const total = num1 + num2;
+  return total;
+}
+const result: number = sum(6, 4);
+console.log(result);
+
